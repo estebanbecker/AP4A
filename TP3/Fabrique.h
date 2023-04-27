f804bf2a5c1387fd7a51443a2bfd8656be3db669
Esteban <git@beckeresteban.eu>
2023-04-27 15:15:40 +0200

    Final push

--- TP3/Fabrique.h
+++ TP3/Fabrique.h
@@ -9,9 +9,13 @@ class Fabrique
 {
     private:
         Liste<Vehicule*> listeVehicule;
+        int nbVoit = 0;
+        int nbCam = 0;
     public:
 
         void production(int nbVoiture, int nbCamion){
+            nbVoit=nbVoiture;
+            nbCam=nbCamion;
             for (int i = 0; i < nbVoiture; i++){
                 listeVehicule.ajouter(new Voiture(), i);
             }
@@ -21,18 +25,62 @@ class Fabrique
         }
 
         void control(){
-            for (int i = 0; i < listeVehicule.longueur(); i++){
-                listeVehicule[i]->control();
+            for (Liste<Vehicule*>::Iterateur it = listeVehicule.begin(); it != listeVehicule.end(); it++){
+                (*it)->control();
             }
         }
 
         void afficher(){
-            for (int i = 0; i < listeVehicule.longueur(); i++){
-                listeVehicule[i]->afficher();
+            for (Liste<Vehicule*>::Iterateur it = listeVehicule.begin(); it != listeVehicule.end(); it++){
+                (*it)->afficher();
             }
         }
 
-        
+        ~Fabrique(){
+            for (Liste<Vehicule*>::Iterateur it = listeVehicule.begin(); it != listeVehicule.end(); it++){
+                delete *it;
+            }
+        }
+
+        Fabrique(Fabrique& f){
+            int i=0;
+            for (Liste<Vehicule*>::Iterateur it = f.listeVehicule.begin(); it != f.listeVehicule.end(); it++){
+                if (i<nbVoit){
+                    listeVehicule.ajouter(new Voiture(), listeVehicule.longueur());
+                }
+                else{
+                    listeVehicule.ajouter(new Camion(), listeVehicule.longueur());
+                }
+            }
+        }
+
+        Fabrique& operator=(Fabrique& f){
+            if(this==&f){
+                return *this;
+            }
+
+            for (Liste<Vehicule*>::Iterateur it = listeVehicule.begin(); it != listeVehicule.end(); it++){
+                delete *it;
+            }
+
+
+
+            int i=0;
+            for (Liste<Vehicule*>::Iterateur it = f.listeVehicule.begin(); it != f.listeVehicule.end(); it++){
+                if (i<nbVoit){
+                    listeVehicule.ajouter(new Voiture(), listeVehicule.longueur());
+                }
+                else{
+                    listeVehicule.ajouter(new Camion(), listeVehicule.longueur());
+                }
+            }
+            return *this;
+        }
+
+        Fabrique(){
+            production(0,0);
+        }
+  
 };
 
 #endif
