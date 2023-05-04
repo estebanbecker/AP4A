58a217a7857de3927f8db8cd4003624d043bd0d5
Esteban <git@beckeresteban.eu>
2023-05-04 09:30:56 +0200

    debugg output

--- TP3/Vehicule.h
+++ TP3/Vehicule.h
@@ -65,7 +65,7 @@ class Estart : public Esystem
                 if( id == -1){
                     std::cout << "Panne de demarrage" << std::endl;
                 }else{
-                    std::cout << "Panne de feux du vehicule " << id << std::endl;
+                    std::cout << "Panne de demarrage du vehicule " << id << std::endl;
                 }
                 return i ;
             }
@@ -95,7 +95,7 @@ class Emoteur : public Esystem
                 if( id == -1){
                     std::cout << "Panne de moteur" << std::endl;
                 }else{
-                    std::cout << "Panne de feux du vehicule " << id << std::endl;
+                    std::cout << "Panne de moteur du vehicule " << id << std::endl;
                 }
                 return i ;
             }
@@ -125,7 +125,7 @@ class Efrein : public Esystem
                 if( id == -1){
                     std::cout << "Panne de frein" << std::endl;
                 }else{
-                    std::cout << "Panne de feux du vehicule " << id << std::endl;
+                    std::cout << "Panne de frein du vehicule " << id << std::endl;
                 }
                 return i ;
             }
